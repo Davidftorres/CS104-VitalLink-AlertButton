0c7c14aa808d7c4b912eb81610466400ae409ba8
David <torresd3@ensign.edu>
2026-04-16 20:11:25 -0600

    6.4 Raspberry Pi Project: Connect with the Telegram API Completed

--- alert_button.py
+++ alert_button.py
@@ -1,13 +1,23 @@
-import time
 import RPi.GPIO as GPIO
+import requests
+import time
+
+TOKEN = "8731466568:AAEXoN3Gwp-7fFsiUW4HUgJ8F4iUOtgrJgE"
+CHAT_ID = "8628434531"
+
+URL = f"https://api.telegram.org/bot8731466568:AAEXoN3Gwp-7fFsiUW4HUgJ8F4iUOtgrJgE/sendMessage"
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-button_pressed = False
 while True:
-    if GPIO.input(7) == GPIO.HIGH and not button_pressed:
+    if GPIO.input(7) == GPIO.HIGH:
         print("Someone pressed the alert button!")
-        button_pressed = True
-    elif GPIO.input(7) == GPIO.LOW:
-        button_pressed = False
-    time.sleep(0.1)
+
+        data = {
+            "chat_id": CHAT_ID,
+            "text": "Someone pressed the alert button!"
+        }
+
+        requests.post(URL, data=data)
+        time.sleep(1)
